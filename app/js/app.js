f7cfb6bd703a72e7eaa531cbd0ba7d3dd35c0d80
roman_kononov <romankononovdev@mail.ru>
2020-10-26 23:50:54 +0700

    finished the price section

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
-
-});
+// ================================= ADAPTIVE MENU ================================ 
 
 document.addEventListener(
 	"DOMContentLoaded", () => {
-			new Mmenu( "#my-menu", {
-					// options
-				extensions: ["position-right", "border-none",] ,
-				'load': true
-			}, {
-					// configuration
-					// classNames: {
-							// selected: "active"
-					// }
-			});
+		new Mmenu("#my-menu", {
+			// options
+			extensions: ["position-right", "border-none", ],
+			'load': true
+		}, {
+			// configuration
+			// classNames: {
+			// selected: "active"
+			// }
+		});
 	}
 );
 
+// ================================================================================== 
+
+
+// ============================ SECTION PRICE ADAPTIVE ===============================
+
+let showContent = document.querySelector('#showContent');
+let hideContent = document.querySelector('#hideContent');
+let hiddenContents = document.querySelectorAll('.hiddenBlock');
+
+showContent.addEventListener('click', function () {
+	for (let content of hiddenContents) {
+		content.style.display = 'flex';
+
+	}
+
+	showContent.style.display = 'none';
+	hideContent.style.display = 'inline-block';
+});
+
+hideContent.addEventListener('click', function () {
+	for (let content of hiddenContents) {
+		content.style.display = 'none';
+	}
+	hideContent.style.display = 'none';
+	showContent.style.display = 'inline-block';
+});
+
+
+// ===================================================================================
