6b2f78f8ef4418b9a15f05fb3a877a667c05712b
roman_kononov <romankononovdev@mail.ru>
2020-11-03 23:39:27 +0700

    save changes to the map section

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,3 @@
-
 // ================================= ADAPTIVE MENU ================================ 
 
 document.addEventListener(
@@ -21,13 +20,13 @@ document.addEventListener(
 
 // ============================ SECTION PRICE ADAPTIVE ===============================
 
-$("#hideContent").click(function(){
+$("#hideContent").click(function () {
 	$(".hiddenBlock").slideUp(1000);
 	$('#showContent').show();
 	$('#hideContent').hide();
 });
 
-$("#showContent").click(function(){
+$("#showContent").click(function () {
 	$(".hiddenBlock").slideDown("slow");
 	$('#hideContent').show();
 	$('#showContent').hide();
@@ -56,25 +55,25 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 
-$(".car-choice__button--1").click(function(e) {
-  e.preventDefault();
-  $(".carPark__slider").removeClass('active');
+$(".car-choice__button--1").click(function (e) {
+	e.preventDefault();
+	$(".carPark__slider").removeClass('active');
 	$('.carPark__slider--1').addClass('active');
 	$('.car-choice__button').removeClass('isactive');
 	$(this).addClass('isactive');
-});	
+});
 
-$(".car-choice__button--2").click(function(e) {
-  e.preventDefault();
-  $(".carPark__slider").removeClass('active');
+$(".car-choice__button--2").click(function (e) {
+	e.preventDefault();
+	$(".carPark__slider").removeClass('active');
 	$('.carPark__slider--2').addClass('active');
 	$('.car-choice__button').removeClass('isactive');
 	$(this).addClass('isactive');
 });
 
-$(".car-choice__button--3").click(function(e) {
-  e.preventDefault();
-  $(".carPark__slider").removeClass('active');
+$(".car-choice__button--3").click(function (e) {
+	e.preventDefault();
+	$(".carPark__slider").removeClass('active');
 	$('.carPark__slider--3').addClass('active');
 	$('.car-choice__button').removeClass('isactive');
 	$(this).addClass('isactive');
@@ -84,22 +83,44 @@ $(".car-choice__button--3").click(function(e) {
 
 // =========================== SECTION REVIEWS READ ALL ==============================
 
-$(".reviews-block__readMoreButton--1").click(function(){
+$(".reviews-block__readMoreButton--1").click(function () {
 	$(".reviews-block__dots--1").hide();
 	$('.reviews-block__readMore--1').show();
 	$(".reviews-block__readMoreButton--1").hide();
 });
 
-$(".reviews-block__readMoreButton--2").click(function(){
+$(".reviews-block__readMoreButton--2").click(function () {
 	$(".reviews-block__dots--2").hide();
 	$('.reviews-block__readMore--2').show();
 	$(".reviews-block__readMoreButton--2").hide();
 });
 
-$(".reviews-block__readMoreButton--3").click(function(){
+$(".reviews-block__readMoreButton--3").click(function () {
 	$(".reviews-block__dots--3").hide();
 	$('.reviews-block__readMore--3').show();
 	$(".reviews-block__readMoreButton--3").hide();
 });
 
 // ===================================================================================
+
+// let map_container = document.getElementById('map_container');
+// let options_map = {
+// 		once: true,//once start, thereafter destroy listener
+// 		passive: true,
+// 		capture: true
+// };
+// map_container.addEventListener('click', start_lazy_map, options_map);
+// map_container.addEventListener('mouseover', start_lazy_map, options_map);
+// map_container.addEventListener('touchstart', start_lazy_map, options_map);
+// map_container.addEventListener('touchmove', start_lazy_map, options_map);
+
+// let map_loaded = false;
+// function start_lazy_map() {
+// 		if (!map_loaded) {
+// 				let map_block = document.getElementById('ymap_lazy');
+// 				map_loaded = true;
+// 				map_block.setAttribute('src', map_block.getAttribute('data-src'));
+// 				map_block.removeAttribute('data_src');
+// 				console.log('loaded');
+// 		}
+// }
