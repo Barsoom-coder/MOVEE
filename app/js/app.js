2267efcf9d8a1efe174f0b19aa22c99d658db17f
roman_kononov <romankononovdev@mail.ru>
2020-11-04 18:15:19 +0700

    finished the map section

--- app/js/app.js
+++ app/js/app.js
@@ -103,24 +103,24 @@ $(".reviews-block__readMoreButton--3").click(function () {
 
 // ===================================================================================
 
-// let map_container = document.getElementById('map_container');
-// let options_map = {
-// 		once: true,//once start, thereafter destroy listener
-// 		passive: true,
-// 		capture: true
-// };
-// map_container.addEventListener('click', start_lazy_map, options_map);
-// map_container.addEventListener('mouseover', start_lazy_map, options_map);
-// map_container.addEventListener('touchstart', start_lazy_map, options_map);
-// map_container.addEventListener('touchmove', start_lazy_map, options_map);
+let map_container = document.getElementById('map_container');
+let options_map = {
+		once: true,//once start, thereafter destroy listener
+		passive: true,
+		capture: true
+};
+map_container.addEventListener('click', start_lazy_map, options_map);
+map_container.addEventListener('mouseover', start_lazy_map, options_map);
+map_container.addEventListener('touchstart', start_lazy_map, options_map);
+map_container.addEventListener('touchmove', start_lazy_map, options_map);
 
-// let map_loaded = false;
-// function start_lazy_map() {
-// 		if (!map_loaded) {
-// 				let map_block = document.getElementById('ymap_lazy');
-// 				map_loaded = true;
-// 				map_block.setAttribute('src', map_block.getAttribute('data-src'));
-// 				map_block.removeAttribute('data_src');
-// 				console.log('loaded');
-// 		}
-// }
+let map_loaded = false;
+function start_lazy_map() {
+		if (!map_loaded) {
+				let map_block = document.getElementById('ymap_lazy');
+				map_loaded = true;
+				map_block.setAttribute('src', map_block.getAttribute('data-src'));
+				map_block.removeAttribute('data_src');
+				console.log('loaded');
+		}
+}
